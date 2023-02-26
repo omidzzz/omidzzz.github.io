8fa2bb68a7b88ec8cce649681e74372565c91575
Sinisteroid <ghadamgahi.omid@gmail.com>
2023-02-26 07:19:42 +0330

    3rd

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,14 @@
 const Header = () => {
 
     return(
-        <div className='topBar'>
-            Navbar
+        <div className='topB'>
+            <div className='navB'>
+                <div className='logoNC'>
+                    <div className='woundedN'></div>
+                </div>
+                <div className='leftNav'></div>
+
+            </div>
         </div>
     )
 }
--- src/store/utils/thunks.js
+++ src/store/utils/thunks.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-const apiUrl = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=simplyeffedup&limit=4&api_key=f59084f6f83edbe693185eb0ec6b2272&format=json';
+const apiUrl = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=simplyeffedup&limit=3&api_key=f59084f6f83edbe693185eb0ec6b2272&format=json';
 
 export const fetchLast = createAsyncThunk(
     'posts/fetchLast',
